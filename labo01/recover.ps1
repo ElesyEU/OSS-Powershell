13f6a095b3861edb315c1e2f6e4d87a4bda2be97
Michael Somers <michael.somers@student.ap.be>
2023-05-10 21:27:31 +0200

    Tried a bunch of stuff, none of which works particularly well.

--- labo01/recover.ps1
+++ labo01/recover.ps1
@@ -7,14 +7,27 @@ if (Test-Path ".\recovered")
 Expand-Archive recovered.zip
 
 if (Test-Path ".\csvfile.csv")
-{Remove-Item .\recovered -Recurse -Force}
+{Remove-Item .\csvfile.csv -Recurse -Force}
 
 $login = "www"
 $dns = Resolve-DnsName _access.dieter.afterthought.be -Type TXT
 $password = $dns.Strings[0]
 $cred = New-Object System.Management.Automation.PSCredential ($login, ($password | ConvertTo-SecureString -AsPlainText -Force))
 
-try { Invoke-WebRequest -Uri https://dieter.afterthought.be/metadata/emoji.csv -Credential $cred -OutFile "C:\Users\vaags\Downloads\csvfile.csv" }
+try { Invoke-WebRequest -Uri https://dieter.afterthought.be/metadata/emoji.csv -Credential $cred -OutFile ".\csvfile.csv" }
 catch { "An error occured while trying to download the .csv file." }
 
-Get-Content csvfile.csv
+Remove-Item .\recovered\README.txt
+
+$csv = Get-Content csvfile.csv
+$csvobject = $csv | ConvertFrom-Csv
+$recovered = Get-ChildItem .\recovered
+
+Write-Host $csvobject | Where-Object { $_.Name, $_.MD5}
+Write-Host $recovered
+
+foreach ($item in $recovered)
+{
+    $filehash = Get-FileHash $item -Algorithm MD5 | Select-Object
+    Write-Host $filehash
+}
