13a1caf4f74c347e4b9ca12df9c75aed5de9e106
Michael Somers <michael.somers@student.ap.be>
2023-05-25 12:36:18 +0200

    Finished script

---
+++ labo03/Oef 4.ps1
@@ -0,0 +1,13 @@
+﻿clear-host
+import-module AzureAD
+$user = "s139965@ap.be"
+Connect-AzureAD -AccountId $user
+$currentUser = Get-AzureADUser -Filter "GivenName eq '$user'"
+$userGroups = Get-AzureADUserMembership -ObjectId $user
+
+#$os = $userGroups | Select-Object -Property DisplayName, Description | Where-Object DisplayName  -eq "22-23 OS Scripting - (AP-187945)"
+#Get-AzureADGroupMember $os
+
+$os = Get-AzureADGroup -Filter "DisplayName eq '22-23 OS Scripting - (AP-187945)'"
+$users = Get-AzureADGroupMember -ObjectId $os.ObjectId | Select-Object -Property DisplayName
+$users.count
