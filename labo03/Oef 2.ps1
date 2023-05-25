c64356cea2757947379288bab3c9209ef37ad698
Michael Somers <michael.somers@student.ap.be>
2023-05-25 12:34:34 +0200

    Finished script

---
+++ labo03/Oef 2.ps1
@@ -0,0 +1,6 @@
+﻿clear-host
+import-module AzureAD
+$user = "s139965@ap.be"
+Connect-AzureAD -AccountId $user
+$currentUser = Get-AzureADUser -Filter "UserPrincipalName eq 's139965@ap.be'"
+$currentUser | Format-List *
